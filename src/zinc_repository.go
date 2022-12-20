14bd566545c6ed234a8d4812a30cc0b76b2081d3
Jorge Gonzalez <jorg3.594@gmail.com>
2022-12-19 22:46:30 -0300

    Add ZincRepository

---
+++ src/zinc_repository.go
@@ -0,0 +1,29 @@
+package indexer
+
+import "log"
+
+type ZincRepository struct {
+	httpClient Http
+}
+
+type Error struct {
+	Message     string `json:"mailId"`
+	RecordCount int    `json:"record_count"`
+}
+
+type documentsBulk struct {
+	Index   string `json:"index"`
+	Records []Mail `json:"records"`
+}
+
+func InitRepository(httpClient Http) *ZincRepository {
+	return &ZincRepository{httpClient: httpClient}
+}
+
+func (r *ZincRepository) PersistEmails(emails []Mail) {
+	documents := documentsBulk{Index: "emails", Records: emails}
+	_, success := r.httpClient.Post("/api/_bulkv2", documents)
+	if !success {
+		log.Println("No se pudo crear el siguiente lotes de emails: ", emails)
+	}
+}
