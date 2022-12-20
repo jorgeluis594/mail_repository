f0acd828b953a8c9c3ad276dc57a367a6f1afb1a
Jorge Gonzalez <jorg3.594@gmail.com>
2022-12-19 22:05:52 -0300

    Add HttpClient

---
+++ src/http_client.go
@@ -0,0 +1,83 @@
+package indexer
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Http interface {
+	Post(path string) ([]byte, error)
+	Get(path string) ([]byte, error)
+}
+
+type HttpClient struct {
+	Host string
+	username string
+	password string
+	client http.Client
+}
+
+func InitHttpClient(host string, username string, password string) *HttpClient {
+	httpClient := HttpClient{
+	  Host: host,
+	  username: username,
+	  password: password,
+	}
+	httpClient.client = http.Client{}
+	return &httpClient
+}
+
+func (c *HttpClient) Post(path string, object *interface{}) ([]byte, bool) {
+	var json *bytes.Buffer
+	if object != nil {
+		jsonData := toJson(object)
+		json = bytes.NewBuffer(jsonData)
+	}
+
+	req, err := http.NewRequest("POST", c.Host + path, json)
+	if err != nil {
+		log.Fatal("cannot make request with url: ", c.Host + path)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	c.setBasicAuth(req)
+	return c.sendRequest(req)
+}
+
+func (c *HttpClient) Get(path string) ([]byte, bool) {
+	req, err := http.NewRequest("GET", c.Host + path, nil)
+	if err != nil {
+		log.Fatal("cannot make request with url: ", c.Host + path)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	c.setBasicAuth(req)
+	return c.sendRequest(req)
+}
+
+func (c *HttpClient) sendRequest(req *http.Request) ([]byte, bool) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		return body, true
+	} else {
+		return body, false
+	}
+}
+
+func (c *HttpClient) setBasicAuth(req *http.Request) {
+	req.SetBasicAuth(c.username, c.password)
+}
+
+func toJson(object *interface{}) []byte {
+	jsonData, err := json.Marshal(object)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return jsonData
+}
